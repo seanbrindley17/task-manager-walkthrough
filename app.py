87c6c6ce1c2049d32d7d67eec5c10ad430fc33fd
Sean Brindley <seanbrindley17@gmail.com>
2024-09-03 15:59:39 +0100

    Add navbar and fixed loading issue

--- app.py
+++ app.py
@@ -26,5 +26,5 @@ def get_tasks():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT", "5000")),
+            port=int(os.environ.get("PORT")),
             debug=True)
